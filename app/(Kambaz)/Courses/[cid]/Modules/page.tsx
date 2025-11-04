27692c15a8a46efe679fff6c814c5d96ce8326f9
MaiAnh-KT <132906105+MaiAnh-KT@users.noreply.github.com>
2025-11-03 19:37:23 -0500

    fixing reducer and RootState errors

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -4,10 +4,9 @@ import { useState } from "react";
 import { useParams } from "next/navigation";
 import { FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
 import { BsGripVertical } from "react-icons/bs";
-import * as db from "../../../Database";
+// import * as db from "../../../Database";
 import ModulesControls from "./ModulesControls";
 import LessonControlButtons from "./LessonControlButtons";
-import { v4 as uuidv4 } from "uuid";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { addModule, editModule, updateModule, deleteModule }
   from "./reducer";
@@ -97,58 +96,3 @@ export default function Modules() {
     </div>
   );
 }
-
-// // /* eslint-disable @typescript-eslint/no-explicit-any */
-// // "use client";
-// // import { useState } from "react";
-// // import { useParams } from "next/navigation";
-// // import { ListGroup, ListGroupItem } from "react-bootstrap";
-// // import { BsGripVertical } from "react-icons/bs";
-// // import * as db from "../../../Database";
-// // import ModulesControls from "./ModulesControls";
-// // import LessonControlButtons from "./LessonControlButtons";
-
-// // export default function Modules() {
-// //   const { cid } = useParams();
-// //   // const modules = db.modules.filter((module: any) => module.id === cid);
-// //   const [modules, setModules] = useState<any[]>(db.modules.filter((module: any) => module.id === cid));
-// //   console.log(modules)
-
-// //   return (
-// //     <div>
-// //       {/* Module controls at the top */}
-// //       <ModulesControls /><br /><br /><br />
-// //       {/* Modules list */}
-// //       <ListGroup className="rounded-0" id="wd-modules">
-// //         {modules.map((module: any) => (
-// //           <ListGroupItem
-// //             key={module.id || module._id}
-// //             className="wd-module p-0 mb-5 fs-5 border-gray"
-// //           >
-// //             <div className="wd-title p-3 ps-2 bg-secondary d-flex align-items-center justify-content-between">
-// //               <span>
-// //                 <BsGripVertical className="me-2 fs-3" /> {module.name}
-// //               </span>
-// //             </div>
-
-// //             {module.lessons && module.lessons.length > 0 && (
-// //               <ListGroup className="wd-lessons rounded-0">
-// //                 {module.lessons.map((lesson: any) => (
-// //                   <ListGroupItem
-// //                     key={lesson.id || lesson._id}
-// //                     className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center"
-// //                   >
-// //                     <span>
-// //                       <BsGripVertical className="me-2 fs-3" /> {lesson.name}
-// //                     </span>
-// //                     <LessonControlButtons />
-// //                   </ListGroupItem>
-// //                 ))}
-// //               </ListGroup>
-// //             )}
-// //           </ListGroupItem>
-// //         ))}
-// //       </ListGroup>
-// //     </div>
-// //   );
-// // }
